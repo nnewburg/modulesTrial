d8b0c86f4ae318465fdbd4ed95e301f3c529141d
Nick <pickme1152019@gmail.com>
2019-03-08 02:27:30 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var list = require("./module");
+
+list.addToList(5);
+console.log(list.getList())
+// var coolMath = require("./module.js");
+
+// console.log(coolMath.PHI);  // log a Number
+
+// coolMath.explain();  // invokes the function `explain`/
---
+++ module.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  num: 45,
+  getList: function(){
+    return arr
+  },
+  addToList: function(x) {
+    arr.push(x)
+  }
+};
+
+arr = []
+
+// module.exports = {
+//   PHI: 1.618,
+//   explain: function() {
+//     console.log("(X+Y) is to X as X is to Y: (X+Y)≈" + this.PHI);
+//   }
+// };
